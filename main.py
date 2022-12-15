9c99a894ddf4a0573c840f974f259c38c6748b43
slathiya444 <slathiya444@gmail.com>
2022-12-15 06:22:21 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import boto3
+import time
+import json
+
+client = boto3.client('timestream-write')
+query_client = boto3.client('timestream-query')
+
+if __name__ == "__main__":
+
+
+    del_table_response = client.delete_table(
+        DatabaseName='DemoDB',
+        TableName='DemoTable'
+    )
+
+    del_response = client.delete_database(
+        DatabaseName="DemoDB"
+    )
+
+    db_response = client.create_database(
+        DatabaseName="DemoDB",
+        Tags=[
+            {
+                'Key': 'string',
+                'Value': 'string'
+            }
+        ]
+    )
+    # print(db_response)
+
+    tbl_response = client.create_table(
+        DatabaseName="DemoDB",
+        TableName="DemoTable"
+    )
+
+    # print(tbl_response)
+
+    write_records=response = client.write_records(
+        DatabaseName='DemoDB',
+        TableName='DemoTable',
+        Records=[
+            {
+                'Dimensions': [
+                    {
+                        'Name': 'id',
+                        'Value': '1'
+                    },
+                ],
+                'MeasureName': 'weight',
+                'MeasureValue': '48',
+                'MeasureValueType': 'DOUBLE',
+                'Time': str(int(round(time.time() * 1000))),
+                'TimeUnit': 'MILLISECONDS'
+            },
+        ]
+    )
+
+    query_response = query_client.query(
+        QueryString=f"SELECT * FROM {db_response['Database']['DatabaseName']}.{tbl_response['Table']['TableName']}"
+    )
+
+    print(json.dumps(query_response, indent=3))
